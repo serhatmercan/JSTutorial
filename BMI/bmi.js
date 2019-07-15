0df2c2d774903cf73ea523ed7bf87cd9c871cd32
Serhat Mercan <serhatmercan67@gmail.com>
2019-07-15 14:37:50 +0300

    Create bmi.js

---
+++ BMI/bmi.js
@@ -0,0 +1,24 @@
+/*
+Challenge-1 : BMI
+
+Mark & John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: 
+BMI = mass / height ^ 2 = mass / ( height * height ). (Mass in kg and Height in meter).
+
+1. Store Mark's & John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a booelan variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the varible from step 3.
+   (Something like "Is Mark's BMI higher than John's ? true").
+*/
+
+var massMark = 65,
+    massJohn = 75,
+    heightMark = 1.75,
+    heightJohn = 1.85;
+    
+var bmiMark = massMark / ( heightMark * heightMark ),
+    bmiJohn = massJohn / ( heightJohn * heightJohn );
+    
+var result = ( bmiMark > bmiJohn );
+
+this.console.log("Is Mark's BMI higher than John's ? " + result);
