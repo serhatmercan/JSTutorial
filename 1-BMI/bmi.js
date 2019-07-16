e2040458c0b900f53dca84cb471c4f6af44010d9
Serhat Mercan <serhatmercan67@gmail.com>
2019-07-16 21:58:36 +0300

    Update bmi.js

--- 1-BMI/bmi.js
+++ 1-BMI/bmi.js
@@ -1,11 +1,15 @@
 var massMark = 65,
-    massJohn = 75,
-    heightMark = 1.75,
-    heightJohn = 1.85;
-    
-var bmiMark = massMark / ( heightMark * heightMark ),
-    bmiJohn = massJohn / ( heightJohn * heightJohn );
-    
-var result = ( bmiMark > bmiJohn );
+	massJohn = 75,
+	heightMark = 1.75,
+	heightJohn = 1.85;
 
-this.console.log("Is Mark's BMI higher than John's ? " + result);
+var bmiMark = massMark / (heightMark * heightMark),
+	bmiJohn = massJohn / (heightJohn * heightJohn);
+
+if (bmiMark > bmiJohn) {
+	this.console.log("Mark's BMI higher than John's");
+} else if (bmiMark === bmiJohn) {
+	this.console.log("Mark's BMI is equal to John's");
+} else {
+	this.console.log("John's BMI higher than Mark's");
+}
